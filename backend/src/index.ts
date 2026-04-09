52d52dbd1f30ad52e666b2301db1906324a3e1f5
Shashkevych Dmytro <shashkevich41@gmail.com>
2026-04-09 17:53:46 +0300

    Modification: All CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,26 +16,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(express.static(join(__dirname, "../public")));
 app.use(express.json({ limit: "16kb" }));
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      const allowedOrigins = [
-        "http://localhost:3535",
-        "https://toon-infrastructure.xyz/notification-service/*",
-      ];
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-);
-
+app.use(cors());
 app.use("/api", subscriptionRouter);
 app.use(errorHandler);
 
